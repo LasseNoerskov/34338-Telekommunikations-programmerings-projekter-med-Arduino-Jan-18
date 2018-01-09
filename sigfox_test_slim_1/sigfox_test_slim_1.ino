9d437ade955dbeb52f4c84babc66d94de1e23cc4
SHC <s123788@student.dtu.dk>
2018-01-09 11:43:06 +0100

    Sigfox librarys, and demo code

---
+++ sigfox_test_slim_1/sigfox_test_slim_1.ino
@@ -0,0 +1,14 @@
+
+#include <WISOL.h>
+
+Isigfox *Isigfox = new WISOL();
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
