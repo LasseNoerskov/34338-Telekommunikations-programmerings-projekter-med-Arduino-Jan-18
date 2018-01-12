69feb730f07a4308f487ad93f6bc8aefb7c1689c
SHC <s123788@student.dtu.dk>
2018-01-12 11:02:03 +0100

    EEPROM implemented. Global time implemented, CHECK behaviour around wraparound time (midnight UTC).

--- SF_GPS_Comb_v1/SF_GPS_Comb_v1.ino
+++ SF_GPS_Comb_v1/SF_GPS_Comb_v1.ino
@@ -1,7 +1,7 @@
-
 #include <WISOL.h>
 #include <SoftwareSerial.h>
 #include <sigfox_Support.h>
+#include <EEPROM.h>
 
 #define USE_SIGFOX 0          // 0 = disable all Sigfox code, 1 = enable Sigfox code
 #define TEST_SIGFOX 1          // 0 = normal operation. 1 = the program will pretend it is sending stuff.
@@ -10,6 +10,13 @@
 #define MOCK_LONG 125168
 #define MOCK_TIME 1034
 
+#define SIGFOX_TRANSMISSION_INTERVAL  900000    // Time between Sigfox transmissions in ms (15m).
+#define GPS_FIX_STD_INTERVAL 30000             // Standard time between GPS rewuests in ms (5m = 300000ms).
+
+#define EEPROM_ADDR_LAT 0          // Address of the Lattitude in the EEPROM.
+#define EEPROM_ADDR_LONG 4         // Address of the Lattitude in the EEPROM.
+#define EEPROM_ADDR_GPS_TIM 8      // Address of the last-GPS-fix-time  in the EEPROM.
+#define EEPROM_ADDR_SF_TIM 12      // Address of the last-Sigfox-Transission-time  in the EEPROM.
 
 SoftwareSerial gpsSerial(A3, A2); // RX, TX
 
@@ -23,8 +30,14 @@ char GPS_info_buffer[GPS_INFO_BUFFER_SIZE];
 unsigned int received_char = 0;
 bool message_started=false;
 
-long prevMillies = 0;       // last time millis() was called.
-long currentTime = 0;       // The current time of day in milli seconds.
+long lastSfSendTime = 0;                    // Last time (of day) the last Sigfox transmission was sent.
+long lastGPSUpdateTime = 0;                 // Last time (of day) the GPS position was successfully updated.
+long latitude;                              // Last valid GPS lat.
+long longtitude;                            // Last valid GPS long.
+long gpsTime;                               // Last valid GPS time.
+
+unsigned long prevMillies = 0;       // last time millis() was called.
+unsigned long currentTime = 0;       // The current Time Of Day in milli seconds. Time is UTC.
 
 void setup() {
   //******************************//
@@ -60,6 +73,18 @@ void setup() {
   gpsSerial.println(F("$PUBX,40,GLL,0,0,0,0*5C")); //GLL OFF
   delay(1000);
 
+  Serial.println(eepromRead( EEPROM_ADDR_LAT));
+  Serial.println(eepromRead( EEPROM_ADDR_LONG));
+  Serial.println(eepromRead( EEPROM_ADDR_GPS_TIM));
+  Serial.println(eepromRead( EEPROM_ADDR_SF_TIM));
+
+  // Reading old values from EEPROM.
+  lastSfSendTime = eepromRead( EEPROM_ADDR_SF_TIM);                     // Last time (of day) the last Sigfox transmission was sent.
+  lastGPSUpdateTime = eepromRead( EEPROM_ADDR_GPS_TIM);                 // Last time (of day) the GPS position was successfully updated.
+  latitude = eepromRead( EEPROM_ADDR_LAT);                              // Last valid GPS lat.
+  longtitude = eepromRead( EEPROM_ADDR_LONG);                           // Last valid GPS long.
+
+  currentTime = lastGPSUpdateTime;              // Setting the starting time to the last GPS update, as it is closer than 0.
   
   gpsSerial.println("End of Setup");
   Serial.println("End of Setup");
@@ -71,22 +96,17 @@ void loop() { // run over and over
   byte payloadData[] = {0,0,0,0,0,0,0,0,0,0,0,0};
   boolean transmitPayloadFlag = false;              // Flag set when transmitting data.
   boolean sfUpdateAllowedTime = false;              // Flag for allowing new Sigfox transmission, based on time since last.
-  long lastSfSendTime = 0;                          // Last time (of day) the last Sigfox transmission was sent.
+  
   
   boolean updateGPS = false;
   boolean validGPS = false;
   boolean gpsUpdateAllowedTime = false;       // Flag for allowing new GPS update, based on time since last.
-  long lastGPSUpdateTime = 0;                 // Last time (of day) the GPS position was successfully updated.
-
-  long latitude;
-  long longtitude;
-  long gpsTime;
+  
   char gpsTTemp[2] = {0,0};
 
-  int32_t debugInt = 0;
+  long debugLong = 0;
 
-  long prevMillies = 0;
-  long currentTime = 0;       // The current time of day in milli seconds.
+  
   
   //byte tHour = 0;
   //byte tMin = 0;
@@ -97,54 +117,46 @@ void loop() { // run over and over
     //******************************//
     //           Serial             //
     //******************************// 
-    // Recieving on the software serial and saving.
-    /*
-    while(gpsSerial.available()) {
-      gpsSerialBuf[gSB] = gpsSerial.read();
-      gpsSerial.write(gpsSerialBuf[gSB]);
-      if(gpsSerialBuf[gSB] == '\n' || gpsSerialBuf[gSB] == '\r'){
-        gSBComp = true;
-      }
-      gSB++;
-    }
-    // Recieving on the normal serial
-    while(Serial.available()) {
-      normSerialBuf[nSB] = Serial.read();
-
-      if(normSerialBuf[nSB] == '\n' || normSerialBuf[nSB] == '\r'){
-        nSBComp = true;
-      }
-      nSB++;
-    }
-
-    // Write full gpsSerial sentence to norm serial
-    if(gSBComp){
-      Serial.write(gpsSerialBuf);
-      gSBComp = false;
-      gSB = 0;
-    }
-    // Write full normSerial sentence to norm serial
-    if(nSBComp){
-      gpsSerial.write(normSerialBuf);
-      nSBComp = false;
-      nSB = 0;
-    }
-    */
+    
 
     //******************************//
     //           Time               //
     //******************************//
-    prevMillies = millis();
-    currentTime += (millis()-prevMillies);
-
     
-    //gpsUpdateAllowedTime
+    updatedTOD();
+    
+    if(currentTime > debugLong+1000){
+      debugLong = currentTime;
+      Serial.print("TOD: ");
+      Serial.print((currentTime));
+      Serial.print(" ");
+      Serial.print(floor((float)currentTime/60/60/1000));
+      Serial.print(" ");
+      Serial.print( floor( (currentTime/60/60/10)%100)/100*60 );
+      Serial.print(" ");
+      Serial.print(floor((currentTime/1000)%60));
+      Serial.print(" ");
+      Serial.println(" ");
+    }
+    
+    
+    //
 
     //******************************//
     //            GPS               //
     //******************************//
+    // If GPS_FIX_STD_INTERVAL time has passed, allow an update of the GPS pos.
+    if(((lastGPSUpdateTime + GPS_FIX_STD_INTERVAL) < currentTime) ){
+      gpsUpdateAllowedTime = true;
+    }
+    // If 'currentTime' is less than 'lastGPSUpdateTime' we have wrapped around at midnight, 
+    // adjust 'lastGPSUpdateTime' to make sure new updates will happen.
+    if( lastGPSUpdateTime > currentTime ){
+      lastGPSUpdateTime = 600000;           // Set 'lastGPSUpdateTime' to 00:10:00.
+    }
    
-    if(Serial.available() ) {      // && gpsUpdateAllowedTime
+    if(Serial.available() || gpsUpdateAllowedTime ) {      // && gpsUpdateAllowedTime
+      gpsUpdateAllowedTime = false;
       updateGPS = true;
       Serial.read();
     }
@@ -169,9 +181,18 @@ void loop() { // run over and over
         lastGPSUpdateTime += (atol(gpsTTemp))*60;    // Adding minutes.
         gpsTTemp[0] = ti[4]; gpsTTemp[1] = ti[5];         // Putting seconds chars into temp var.
         lastGPSUpdateTime += (atol(gpsTTemp));       // Adding seconds.
-        prevMillies = millis();
-        currentTime = lastGPSUpdateTime*1000;       // Setting time of day var to the gps time.
+        lastGPSUpdateTime *= 1000;                   // converting into milli seconds.
         
+        // Using GPS time to set current TOD.
+        prevMillies = millis();
+        currentTime = lastGPSUpdateTime;       // Setting time of day var to the gps time.
+
+        // Saving latest values in the EEPROM.
+        eepromWrite(latitude, EEPROM_ADDR_LAT);
+        eepromWrite(longtitude, EEPROM_ADDR_LONG);
+        eepromWrite(lastGPSUpdateTime, EEPROM_ADDR_GPS_TIM);
+        
+        // printing studd, used for debugging.
         Serial.println("Valid GPS found");
         Serial.print("Latitude: ");
         Serial.print(la);
@@ -224,8 +245,11 @@ void loop() { // run over and over
 
       if(USE_SIGFOX){
         Send_Pload(payloadData,12);
+        updatedTOD();
         lastSfSendTime = currentTime;
       }
+      eepromWrite(lastSfSendTime, EEPROM_ADDR_SF_TIM);
+      
       if(TEST_SIGFOX){
         gpsSerial.println("\nPAYLOAD: ");
         gpsSerial.print("Data [HEX]: ");
@@ -235,14 +259,14 @@ void loop() { // run over and over
         }
         gpsSerial.print("\n\r");
         gpsSerial.print("LAT: ");
-        debugInt = (((long)payloadData[0])<<16) + (((long)payloadData[1])<<8) + payloadData[2];
-        gpsSerial.print(debugInt); gpsSerial.print("  ");
+        debugLong = (((long)payloadData[0])<<16) + (((long)payloadData[1])<<8) + payloadData[2];
+        gpsSerial.print(debugLong); gpsSerial.print("  ");
         gpsSerial.print("LONG: ");
-        debugInt = (((long)payloadData[3])<<16) + (((long)payloadData[4])<<8) + payloadData[5];
-        gpsSerial.print(debugInt); gpsSerial.print("  ");
+        debugLong = (((long)payloadData[3])<<16) + (((long)payloadData[4])<<8) + payloadData[5];
+        gpsSerial.print(debugLong); gpsSerial.print("  ");
         gpsSerial.print("TIME: ");
-        debugInt = (((long)payloadData[6])<<8) + payloadData[7];
-        gpsSerial.print(debugInt); gpsSerial.print("  ");
+        debugLong = (((long)payloadData[6])<<8) + payloadData[7];
+        gpsSerial.print(debugLong); gpsSerial.print("  ");
 
         gpsSerial.print("\n\r");
       }
@@ -250,6 +274,14 @@ void loop() { // run over and over
     
     
   }
+}
+
+// Updates the global time variable: Current time and prevTime.
+// This fuction adds the time since last call to the 'currentTime'. The time is Time of Day in ms.
+void updatedTOD(){
+  currentTime += (millis()-prevMillies);
+  prevMillies = millis();
+  
 }
 
 // Fetches GPS coordinates, and updates global containers, la[6], lo[7], ti[6].
@@ -315,4 +347,47 @@ void fetch() {
   //Serial.println("done fetching");
 }
 
+// Writes a long to the EEPROM at adress 'addr'.
+// Return 1 on succes, returns -1 if addr is out of range.
+//https://playground.arduino.cc/Code/EEPROMReadWriteLong
+int eepromWrite(long value , unsigned int addr)
+{
+    
+    if (addr >= EEPROM.length()) {
+      return -1;
+    }
+    
+    //Decomposition from a long to 4 bytes by using bitshift.
+    //One = Most significant -> Four = Least significant byte
+    byte four = (value & 0xFF);
+    byte three = ((value >> 8) & 0xFF);
+    byte two = ((value >> 16) & 0xFF);
+    byte one = ((value >> 24) & 0xFF);
+    
+    //Write the 4 bytes into the eeprom memory.
+    EEPROM.write(addr, four);
+    EEPROM.write(addr + 1, three);
+    EEPROM.write(addr + 2, two);
+    EEPROM.write(addr + 3, one);
+    return 1;
+}
+
+// Reads a long from the EEPROM at adress 'addr'.
+// Return the read value on succes, returns 0 if addr is out of range.
+//https://playground.arduino.cc/Code/EEPROMReadWriteLong
+long eepromRead( unsigned int addr )
+{   
+    if (addr >= EEPROM.length()) {
+      return 0;
+    }
+    
+    //Read the 4 bytes from the eeprom memory.
+    long four = EEPROM.read(addr);
+    long three = EEPROM.read(addr + 1);
+    long two = EEPROM.read(addr + 2);
+    long one = EEPROM.read(addr + 3);
+    
+    //Return the recomposed long by using bitshift.
+    return ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
+}
 
